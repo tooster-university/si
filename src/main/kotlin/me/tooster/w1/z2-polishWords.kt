a4b57dd4ec3d85d1d5fb60d8d84ca43c7bdd91df
Tooster <max@polarczyk.pl>
2021-03-01 21:34:43 +0100

    Complete W01-Z02 - splitting spaceless text

--- src/main/kotlin/me/tooster/w1/z2-polishWords.kt
+++ src/main/kotlin/me/tooster/w1/z2-polishWords.kt
@@ -1,2 +1,53 @@
 package me.tooster.w1
 
+import java.io.File
+
+// IDEA 1 (too bothersome): write trie with node visitor, traverse text for each letter read constructing prefix, check
+// if word is full, keep track of 3 node visitors - whenever whole word is found, check consult with other visitors if the split
+// has a meaning
+
+// IDEA 2 (slightly slower): keep words in hashmap, if substring is a word save it as potential split point. Keep
+// track of split points. Afterwards check best split. Approx. avg num of words per line should be around 5-10
+
+const val RESOURCE_PREFIX = "src/main/resources/w1/";
+
+// optional args: <words file> <input file> <output file>
+fun main(args: Array<String>) {
+
+
+    val wordsPath = RESOURCE_PREFIX + args.getOrElse(0) { "polish_words.txt" }
+    val inputPath = RESOURCE_PREFIX + args.getOrElse(1) { "zad2_input.txt" }
+    val outputPath = RESOURCE_PREFIX + args.getOrElse(2) { "zad2_output.txt" }
+
+    val words = File(wordsPath).bufferedReader().useLines { it.toHashSet() }
+
+    File(inputPath).bufferedReader().useLines { input ->
+        File(outputPath).printWriter().use { output ->
+            input.forEach { output.println(split(it, words)) }
+        }
+    }
+
+}
+
+// BestSplit(i) = max(BestSplit(j<i))
+fun split(line: String, words: HashSet<String>): String {
+    val dp = Array(line.length + 1) { idx -> 0 to idx } // <valid split's score ending before i, word start>
+    (1..line.length).forEach { end ->
+        (0 until end).forEach { start ->
+            val word = line.substring(start, end)
+            if (
+                (start == 0 || dp[start].first > 0) && // consider only valid previous splits
+                words.contains(word) &&
+                dp[end].first < dp[start].first + word.length * word.length
+            )
+                dp[end] = dp[start].first + word.length * word.length to start
+        }
+    }
+    var end = dp.size - 1
+    val wordsReversed = mutableListOf<String>()
+    while (end > 0) {
+        wordsReversed.add(line.substring(dp[end].second, end))
+        end = dp[end].second
+    }
+    return wordsReversed.asReversed().joinToString(" ")
+}
